bc614e23c90d2094c8765d7ecc78f1bbad6f487d
Maksym Tsymbal <tsimbal.max94@gmail.com>
2016-03-08 01:46:46 +0200

    Created level prototype. Player can come inside the building and go out.

---
+++ LikeIT16test/Assets/Move.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+	public float speed;
+	Vector3 move;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+		transform.position += move * speed * Time.deltaTime;
+	}
+}
