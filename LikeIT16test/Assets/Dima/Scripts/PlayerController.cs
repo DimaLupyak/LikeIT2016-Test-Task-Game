72ec408b39a5612411ca964c02f0458225025af9
Dima Lupyak <dima.lupyak@gmail.com>
2016-03-08 02:37:40 +0200

    Added player fliping

--- LikeIT16test/Assets/Dima/Scripts/PlayerController.cs
+++ LikeIT16test/Assets/Dima/Scripts/PlayerController.cs
@@ -4,9 +4,10 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float speed = 100;
-    private Rigidbody2D rigi;	
+    private Rigidbody2D rigi;
+    public bool isFacingRight = false;
 
-	void Start()
+    void Start()
 	{
         rigi = GetComponent<Rigidbody2D> ();
 	}
@@ -17,9 +18,21 @@ public class PlayerController : MonoBehaviour {
 		float moveVertical = Input.GetAxis ("Vertical");
         rigi.AddForce(new Vector2(moveHorizontal * speed, moveVertical * speed));
         if(rigi.velocity.x!=0|| rigi.velocity.y != 0) rigi.AddForce(new Vector2(-rigi.velocity.x*10, -rigi.velocity.y*10));
+        if (moveHorizontal > 0 && !isFacingRight)
+            Flip();
+        else if (moveHorizontal < 0 && isFacingRight)
+            Flip();
     }
 
-	void OnTriggerEnter2D(Collider2D other) 
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
 	{
 	}
 }
