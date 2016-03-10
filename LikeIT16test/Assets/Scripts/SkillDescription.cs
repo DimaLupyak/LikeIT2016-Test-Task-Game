0736ddf802c4323afc0f37274f3648a25db0f70d
Maksym Tsymbal <tsimbal.max94@gmail.com>
2016-03-11 01:11:05 +0200

    Upgrade Update

--- LikeIT16test/Assets/Scripts/SkillDescription.cs
+++ LikeIT16test/Assets/Scripts/SkillDescription.cs
@@ -13,13 +13,13 @@ public class SkillDescription : MonoBehaviour {
 		switch(SkillName)
 		{
 		case "guitar":
-			this.desText.text = string.Format("Дає можливість на певний час приспати ворогів. Зараз: {0} секунд, наступний рівень {1} секунд",_base.skills[0].powerValue[PlayerPrefs.GetInt("guitarLVL")],_base.skills[0].powerValue[PlayerPrefs.GetInt("guitarLVL")+1]);
+			this.desText.text = string.Format("Дає можливість на певний час приспати ворогів. Зараз: {0} секунд, наступний рівень {1} секунд",_base.skills[1].powerValue[SaveManager.Instance.GetSkillLevel (SkillType.Guitar)],_base.skills[1].powerValue[SaveManager.Instance.GetSkillLevel (SkillType.Guitar)+1]);
 			break;
 		case "hummer":
-			this.desText.text = string.Format("Наносить певну кількість шкоди ворогам. Зараз: {0} одиниць шкоди, наступний рівень {1} одиниць шкоди",_base.skills[1].powerValue[PlayerPrefs.GetInt("hummerLVL")],_base.skills[1].powerValue[PlayerPrefs.GetInt("hummerLVL")+1]);
+			this.desText.text = string.Format("Наносить певну кількість шкоди ворогам. Зараз: {0} одиниць шкоди, наступний рівень {1} одиниць шкоди",_base.skills[2].powerValue[SaveManager.Instance.GetSkillLevel (SkillType.Hammer)],_base.skills[2].powerValue[SaveManager.Instance.GetSkillLevel (SkillType.Hammer)+1]);
 			break;
 		case "shower":
-			this.desText.text = string.Format("Робить Афанасія невидимим для ворогів на певний час. Зараз: {0} секунд, наступний рівень {1} секунд",_base.skills[2].powerValue[PlayerPrefs.GetInt("showerLVL")],_base.skills[2].powerValue[PlayerPrefs.GetInt("showerLVL")+1]);
+			this.desText.text = string.Format("Робить Афанасія невидимим для ворогів на певний час. Зараз: {0} секунд, наступний рівень {1} секунд",_base.skills[0].powerValue[SaveManager.Instance.GetSkillLevel (SkillType.Curtain)],_base.skills[0].powerValue[SaveManager.Instance.GetSkillLevel (SkillType.Curtain)+1]);
 			break;
 		default:
 			break;
